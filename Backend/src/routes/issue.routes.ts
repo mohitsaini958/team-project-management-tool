b4d5385b519d144c243f6d3fb9e5548207935898
Mohit Saini <mohitsaini9581@gmail.com>
2026-08-10 16:57:23 +0530

    added activity log and comments funcnality

--- Backend/src/routes/issue.routes.ts
+++ Backend/src/routes/issue.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import {createIssueSchema,updateIssueSchema} from "../schema/issue.schema.js"
+import {createIssueSchema,updateIssueSchema,reorderIssueSchema} from "../schema/issue.schema.js"
 
-import {create,update,remove,getByProject} from "../controllers/issue.controller.js"
+import {create,update,remove,getByProject,reorder, getById} from "../controllers/issue.controller.js"
 
 const router=Router();
 router.use(authenticate);
 
 router.post("/",validate(createIssueSchema),create);
+router.patch("/reorder",validate(reorderIssueSchema),reorder);
+router.get("/:issueId",getById);
 router.patch("/:issueId",validate(updateIssueSchema),update);
 router.delete("/:issueId",remove);
 router.get("/project/:projectId", getByProject);
--- Backend/src/schema/issue.schema.ts
+++ Backend/src/schema/issue.schema.ts
@@ -7,30 +7,35 @@ export const createIssueSchema = z.object({
 
   priority: z.enum(["LOW", "MEDIUM", "HIGH", "URGENT"]).optional(),
 
-  status: z
-    .enum([
-      "BACKLOG",
-      "IN_PROGRESS",
-      "IN_REVIEW",
-      "DONE",
-    ])
-    .optional(),
+  status: z.enum(["BACKLOG", "IN_PROGRESS", "IN_REVIEW", "DONE"]).optional(),
 
   assigneeId: z.string().cuid().optional(),
 
   projectId: z.string().cuid(),
 });
 
-export const updateIssueSchema=z.object({
-  title:z.string().trim().min(3).max(200).optional(),
-  description:z.string().optional(),
-  priority:z.enum(["LOW","MEDIUM","HIGH","URGENT"]).optional(),
+export const updateIssueSchema = z.object({
+  title: z.string().trim().min(3).max(200).optional(),
+  description: z.string().optional(),
+  priority: z.enum(["LOW", "MEDIUM", "HIGH", "URGENT"]).optional(),
 
-  status:z.enum(["BACKLOG","IN_PROGRESS","IN_REVIEW","DONE"]).optional(),
+  status: z.enum(["BACKLOG", "IN_PROGRESS", "IN_REVIEW", "DONE"]).optional(),
 });
 
-export type CreateIssueInput =
-  z.infer<typeof createIssueSchema>;
+export const reorderIssueSchema = z.object({
+  issues: z
+    .array(
+      z.object({
+        id: z.string().cuid(),
+        status: z.enum(["BACKLOG", "IN_PROGRESS", "IN_REVIEW", "DONE"]),
+        order: z.number().int().min(0),
+      }),
+    )
+    .min(1),
+});
 
-export type UpdateIssueInput=  
-  z.infer<typeof updateIssueSchema>;
+export type ReorderIssueInput=z.infer<typeof reorderIssueSchema>;
+
+export type CreateIssueInput = z.infer<typeof createIssueSchema>;
+
+export type UpdateIssueInput = z.infer<typeof updateIssueSchema>;
